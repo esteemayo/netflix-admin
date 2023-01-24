9588c272c03a6a8aa83972de74e31ce1a59b673f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-24 22:53:12 +0100

    fix: modify

--- src/redux/movie/movieSlice.js
+++ src/redux/movie/movieSlice.js
@@ -84,77 +84,6 @@ const initialState = {
 export const movieSlice = createSlice({
   name: 'movies',
   initialState,
-  reducers: {
-    getMoviesStart: (state) => {
-      state.isFetching = true;
-    },
-    getMoviesSuccess: (state, { payload }) => {
-      state.movies = payload;
-      state.isFetching = false;
-    },
-    getMoviesFailure: (state) => {
-      state.error = true;
-      state.isFetching = false;
-    },
-    getMovieByIdStart: (state) => {
-      state.isFetching = true;
-    },
-    getMovieByIdSuccess: (state, { payload }) => {
-      state.movie = payload;
-      state.isFetching = false;
-    },
-    getMovieByIdFailure: (state) => {
-      state.error = true;
-      state.isFetching = false;
-    },
-    getMovieBySlugStart: (state) => {
-      state.isFetching = true;
-    },
-    getMovieBySlugSuccess: (state, { payload }) => {
-      state.movie = payload;
-      state.isFetching = false;
-    },
-    getMovieBySlugFailure: (state) => {
-      state.error = true;
-      state.isFetching = false;
-    },
-    createMovieStart: (state) => {
-      state.isFetching = true;
-    },
-    createMovieSuccess: (state, { payload }) => {
-      state.movies.push(payload);
-      state.isFetching = false;
-    },
-    createMovieFailure: (state) => {
-      state.error = true;
-      state.isFetching = false;
-    },
-    updateMovieStart: (state) => {
-      state.isFetching = true;
-    },
-    updateMovieSuccess: (state, { payload }) => {
-      state.movies[state.movies.findIndex((item) => item._id === payload.id)] =
-        payload.movie;
-      state.isFetching = false;
-    },
-    updateMovieFailure: (state) => {
-      state.error = true;
-      state.isFetching = false;
-    },
-    deleteMovieStart: (state) => {
-      state.isFetching = true;
-    },
-    deleteMovieSuccess: (state, { payload }) => {
-      state.movies.splice(
-        state.movies.findIndex((movie) => movie._id === payload),
-        1
-      );
-    },
-    deleteMovieFailure: (state) => {
-      state.error = true;
-      state.isFetching = false;
-    },
-  },
   extraReducers: {
     [fetchMovies.pending]: (state) => {
       state.isFetching = true;
@@ -235,26 +164,5 @@ export const movieSlice = createSlice({
     },
   },
 });
-
-export const {
-  createMovieFailure,
-  createMovieStart,
-  createMovieSuccess,
-  deleteMovieFailure,
-  deleteMovieStart,
-  deleteMovieSuccess,
-  getMovieByIdFailure,
-  getMovieByIdStart,
-  getMovieByIdSuccess,
-  getMovieBySlugFailure,
-  getMovieBySlugStart,
-  getMovieBySlugSuccess,
-  getMoviesFailure,
-  getMoviesStart,
-  getMoviesSuccess,
-  updateMovieFailure,
-  updateMovieStart,
-  updateMovieSuccess,
-} = movieSlice.actions;
 
 export default movieSlice.reducer;
