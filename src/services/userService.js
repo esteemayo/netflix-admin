14e9587aca22559c99f36ea9c3cda051618c630b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-25 00:11:32 +0100

    fix: modify

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ import http from './httpService';
 
 const apiEndpoint = '/users';
 
-const userUrl = (id) => `${apiEndpoint}/${id}`;
+const userUrl = (userId) => `${apiEndpoint}/${userId}`;
 
 export const getAllUsers = () => http.get(apiEndpoint);
 
@@ -10,10 +10,12 @@ export const getUsers = () => http.get(`${apiEndpoint}?new=true`);
 
 export const getUsersStats = () => http.get(`${apiEndpoint}/stats`);
 
+export const getUser = (userId) => http.get(userUrl(userId));
+
 export const register = (credentials) =>
   http.post(`${apiEndpoint}/register`, credentials);
 
-export const editUser = (id, credentials) =>
-  http.patch(userUrl(id), credentials);
+export const editUser = (userId, credentials) =>
+  http.patch(userUrl(userId), credentials);
 
-export const deleteUser = (id) => http.delete(userUrl(id));
+export const deleteUser = (userId) => http.delete(userUrl(userId));
