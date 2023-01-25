ad23eb9fece167698edfd3b2397c36d7b1622799
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-25 06:26:40 +0100

    fix: fix a bug

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from 'redux/user/userSlice';
 import listReducer from 'redux/list/listSlice';
 import movieReducer from 'redux/movie/movieSlice';
-import darkModeReducer from 'redux/movie/darkModeSlice';
+import darkModeReducer from 'redux/darkMode/darkModeSlice';
 
 const store = configureStore({
   reducer: {
