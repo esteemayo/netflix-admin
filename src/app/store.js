ea15a887d884cfef155ae620b111351855132001
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-25 08:29:44 +0100

    feat: configure redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import userReducer from 'redux/user/userSlice';
 import listReducer from 'redux/list/listSlice';
 import movieReducer from 'redux/movie/movieSlice';
 import darkModeReducer from 'redux/darkMode/darkModeSlice';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    lists: listReducer,
-    movies: movieReducer,
-    darkMode: darkModeReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  lists: listReducer,
+  movies: movieReducer,
+  darkMode: darkModeReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { persistor, store };
