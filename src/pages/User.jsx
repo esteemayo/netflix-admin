b0b5ea5c43eb5852d18fd49a7fc5e1c80d66e058
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-25 06:27:43 +0100

    fix: handle dark/light mode

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -314,7 +314,7 @@ const FormInput = styled.input`
   display: block;
   font-size: 1.5rem;
   font-family: inherit;
-  background-color: var(--color-white);
+  background-color: ${({ theme }) => theme.bgInput};
   color: #999;
   padding: 1.5rem 2rem;
   width: 40rem;
