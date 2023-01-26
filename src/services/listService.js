620e8f1079333a604d8fd664d478123ddb02bd2a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-26 13:35:26 +0100

    fix: modify

--- src/services/listService.js
+++ src/services/listService.js
@@ -2,14 +2,15 @@ import http from './httpService';
 
 const apiEndpoint = '/lists';
 
-const listUrl = (id) => `${apiEndpoint}/${id}`;
+const listUrl = (listId) => `${apiEndpoint}/${listId}`;
 
 export const getLists = () => http.get(`${apiEndpoint}`);
 
-export const getList = (id) => http.get(listUrl(id));
+export const getList = (listId) => http.get(listUrl(listId));
 
 export const createList = (list) => http.post(apiEndpoint, list);
 
-export const editList = (id, list) => http.patch(listUrl(id), list);
+export const editList = (listId, list) =>
+  http.patch(listUrl(listId), list);
 
-export const deleteList = (id) => http.delete(listUrl(id));
+export const deleteList = (listId) => http.delete(listUrl(listId));
