6d9476edbf841d9329a9fe3c747d7daca3de6495
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-12 16:19:59 +0100

    feat: add userInputs

---
+++ src/formData.js
@@ -0,0 +1,37 @@
+export const userInputs = [
+  {
+    id: 'username',
+    type: 'text',
+    name: 'username',
+    label: 'Username',
+    placeholder: 'Username',
+  },
+  {
+    id: 'name',
+    type: 'text',
+    name: 'name',
+    label: 'Full Name',
+    placeholder: 'Full Name',
+  },
+  {
+    id: 'email',
+    type: 'email',
+    name: 'email',
+    label: 'Email',
+    placeholder: 'Email',
+  },
+  {
+    id: 'telephone',
+    type: 'tel',
+    name: 'telephone',
+    label: 'Telephone',
+    placeholder: 'Telephone',
+  },
+  {
+    id: 'address',
+    type: 'text',
+    name: 'address',
+    label: 'Address',
+    placeholder: 'Address',
+  },
+];
