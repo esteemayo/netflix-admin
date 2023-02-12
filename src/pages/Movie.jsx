fe45ec7f941db4a777c1c9e44ce53908f55d91c7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-12 15:17:03 +0100

    feat: create a wrapper element

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -406,6 +406,11 @@ const InfoBottom = styled.div`
   margin-top: 1rem;
 `;
 
+const InfoBottonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const InfoItem = styled.div`
   width: 15rem;
   display: flex;
