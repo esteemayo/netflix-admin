f8092e8596d96e0d40ce5bc0b49a72275d3e9099
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-13 02:09:04 +0100

    fix: import useSelector

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useDispatch, } from 'react-redux';
+import { useDispatch, useSelector, } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
