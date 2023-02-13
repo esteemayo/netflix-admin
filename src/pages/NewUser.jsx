5c04ee5328c340b1dcd3a17b29089837e29e3c85
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-13 01:03:30 +0100

    fix: refactor

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -66,44 +66,12 @@ const NewUser = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const fileName = `${new Date().getTime()}-${file.name}`;
+    const userData = {
+      ...inputs,
+    };
 
-    const storage = getStorage(app);
-    const storageRef = ref(storage, `/users/${fileName}`);
-
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      'state_changed',
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-          default:
-        }
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          const userData = {
-            ...inputs,
-            avatar: downloadURL,
-          };
-
-          dispatch(registerUser({ credentials: userData, toast }));
-          navigate('/users');
-        });
-      }
-    );
+    dispatch(registerUser({ credentials: userData, toast }));
+    navigate('/users');
   };
 
   useEffect(() => {
