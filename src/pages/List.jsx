227a7aaebfea512919e19d47379a42904eb8f3e5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-13 02:11:17 +0100

    fix: modify

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
