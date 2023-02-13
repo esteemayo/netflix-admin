d245e9cafe8249650ad11f7b6c9abd2ab728a62f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-14 00:31:35 +0100

    feat: import useEffect react hook

--- src/pages/NewMovie.jsx
+++ src/pages/NewMovie.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
