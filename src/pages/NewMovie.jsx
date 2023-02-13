0e1654df9dd2d6527759a0b19479ca1023213053
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-14 00:32:46 +0100

    fix: modify

--- src/pages/NewMovie.jsx
+++ src/pages/NewMovie.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
