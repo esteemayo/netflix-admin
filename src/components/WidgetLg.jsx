f09427d2a011a7544e6cf639cecf537e4c915141
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-14 01:38:58 +0100

    fix: add alt attribute to img element

--- src/components/WidgetLg.jsx
+++ src/components/WidgetLg.jsx
@@ -61,7 +61,7 @@ const WidgetLg = () => {
 
           <TableRow>
             <TableDataUser>
-              <Image src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+              <Image src='https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' />
               <UserName>Susan carol</UserName>
             </TableDataUser>
             <TableDataDate>2 Jun 2021</TableDataDate>
