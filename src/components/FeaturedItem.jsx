bad473f99370aea840c8bb5681b72c575fcf5ed9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-15 01:32:30 +0100

    fix: render data based on type prop

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,7 +1,59 @@
 import styled from 'styled-components';
 import { ArrowDownward, ArrowUpward } from '@material-ui/icons';
 
-const FeaturedItem = () => {
+const FeaturedItem = ({ type }) => {
+  let data;
+
+  switch (type) {
+    case 'revenue':
+      data = {
+        title: 'Revenue',
+        money: '2,415',
+        rate: '-11.4',
+        icon: (
+          <ArrowDownward style={{
+            fontSize: '1.4rem',
+            marginLeft: '0.5rem',
+            color: 'red',
+          }} />
+        ),
+      };
+      break;
+
+    case 'sales':
+      data = {
+        title: 'Sales',
+        money: '4,415',
+        rate: '-1.4',
+        icon: (
+          <ArrowDownward style={{
+            fontSize: '1.4rem',
+            marginLeft: '0.5rem',
+            color: 'red',
+          }} />
+        ),
+      };
+      break;
+
+    case 'cost':
+      data = {
+        title: 'Cost',
+        money: '2,415',
+        rate: '2.4',
+        icon: (
+          <ArrowUpward style={{
+            fontSize: '1.4rem',
+            marginLeft: '0.5rem',
+            color: 'green',
+          }} />
+        ),
+      };
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <div>FeaturedItem</div>
   );
