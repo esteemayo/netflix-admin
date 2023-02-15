2e84a6be5ca345ac63a4934ab0c02b55f4dcff0f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-15 02:00:56 +0100

    fix: add css property

--- src/pages/NewList.jsx
+++ src/pages/NewList.jsx
@@ -143,6 +143,7 @@ const Input = styled.input`
   display: block;
   width: 40rem;
   padding: 1rem 2rem;
+  padding-left: 1rem;
   font-size: 1.2rem;
   font-family: inherit;
   background-color: transparent;
