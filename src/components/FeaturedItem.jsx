3e5b42055e78e020f560f2bda7120a6cf69d6be9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-15 01:33:09 +0100

    fix: style component

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -59,4 +59,44 @@ const FeaturedItem = ({ type }) => {
   );
 };
 
+const Container = styled.div`
+  flex: 1;
+  font-size: 1.6rem;
+  font-weight: 300;
+  margin: 0 2rem;
+  padding: 3rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  -webkit-box-shadow: ${({ theme }) => theme.box};
+  -moz-box-shadow: ${({ theme }) => theme.box};
+  box-shadow: ${({ theme }) => theme.box};
+`;
+
+const Title = styled.span`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const FeaturedMoneyContainer = styled.div`
+  margin: 1rem 0;
+  display: flex;
+  align-items: center;
+`;
+
+const FeaturedMoney = styled.span`
+  font-size: 3rem;
+  font-weight: 600;
+`;
+
+const FeaturedMoneyRate = styled.span`
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+`;
+
+const FeaturedSub = styled.span`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
 export default FeaturedItem;
