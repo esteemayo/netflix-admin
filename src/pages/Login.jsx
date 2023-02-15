28019620c273530b088cee651f6d8ffe08899357
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-15 17:45:24 +0100

    feat: add Visibility/VisibilityOff icons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 
 import { loginInputs } from 'formData';
 import { loginUser, reset } from 'redux/user/userSlice';
