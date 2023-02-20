f32c9d518dfa4c6f85c4be7c74ad44aa4883c000
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-20 15:49:03 +0100

    fix: modify

--- src/components/WidgetLg.jsx
+++ src/components/WidgetLg.jsx
@@ -27,7 +27,7 @@ const WidgetLg = () => {
                   <UserName>{customer}</UserName>
                 </TableDataUser>
                 <TableDataDate>{createdAt}</TableDataDate>
-                <TableDataAmount>${total}</TableDataAmount>
+                <TableDataAmount></TableDataAmount>
                 <TableDataStatus>
                   <Button type={status}>{status}</Button>
                 </TableDataStatus>
