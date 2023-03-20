08b6dff58f7425862c661a5ba967e93d7eb2d747
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-20 16:13:11 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthRoute from 'utils/AuthRoute';
 import ProtectedRoute from 'utils/ProtectedRoute';
 import { darkTheme, lightTheme } from 'utils/Theme';
+import ProtectedRoute from 'utils/ProtectedRoute';
 import { listColumns, movieColumns, userColumns } from 'data';
 import {
   Home,
