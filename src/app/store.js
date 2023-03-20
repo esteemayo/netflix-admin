40725d563b7f47cf3178fd8cc3ebccf7a8e859ae
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-20 16:10:41 +0100

    fix: modify

--- src/app/store.js
+++ src/app/store.js
@@ -12,7 +12,6 @@ import {
 } from 'redux-persist';
 
 import userReducer from 'redux/user/userSlice';
-import listReducer from 'redux/list/listSlice';
 import movieReducer from 'redux/movie/movieSlice';
 import listReducer from 'redux/list/listSlice';
 import darkModeReducer from 'redux/darkMode/darkModeSlice';
