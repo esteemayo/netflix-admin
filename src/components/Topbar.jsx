e39712d20c6ca9fd5769dac99ce7604c41cd4393
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-20 16:00:55 +0100

    fix: add new css properties

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -150,6 +150,10 @@ const Options = styled.div`
     border-left: 0.5rem solid transparent;
     border-right: 0.5rem solid transparent;
     border-bottom: 0.5rem solid ${({ theme }) => theme.bgOption};
+    position: absolute;
+    top: -0.5rem;
+    left: 33%;
+    transform: translateX(-33%);
   }
 `;
 
